da17c8f0ced63138b4e7a83fa1bf7476327e9375
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-10-09 19:21:18 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan18-Kelipatan/src/if1/pkg10118005/latihan18/kelipatan/IF110118005Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan18.kelipatan;
+
+/**
+ *
+ * @author USER
+ * NAMA : Rycho Rantung
+ * KELAS : IF1
+ * NIM : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan bilangan kelipatan 3.5, dari 3.5 sampai 35
+ */
+public class IF110118005Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double i;
+        for (i = 3.5; i <= 35.0; i+=3.5)
+			System.out.println(i);
+    }
+    
+}
